6c9f2d3e786cd99a8d1df8b4f57ba8ac36b49135
yuli-lynx <bakalovayuliia@gmail.com>
2025-05-17 18:19:46 +0100

    add post.html, create.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 import os
@@ -16,6 +16,35 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    blog_post = BlogPost.query.get_or_404(post_id)
+    return render_template("post.html", post=blog_post)
+
+@app.route("/create", methods=["GET", "POST"])
+def create_post():
+    admin_password = "Password5813!"
+
+    if request.method == "POST":
+        if request.form.get("password") != admin_password:
+            flash("!!!wrongue passwordue!!!")
+            return redirect(url_for("create_post"))
+        
+        title = request.form["title"]
+        content = request.form["content"]
+        hashtags = request.form.get("hashtags")
+
+        if not title or not content:
+            flash("no no no, titullo und contento should be presento!!!")
+            return redirect(url_for("create_post"))
+        
+        new_post = BlogPost(title=title, content=content, hashtags=hashtags)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect(url_for("home"))
+    
+    return render_template("create.html")
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
